2efe99aca26ee16a26fb66f937fb2407ef016eaa
TheSBGames <thesdgams07@gmail.com>
2025-07-03 21:29:38 +0530

    Update config.js

--- config.js
+++ config.js
@@ -4,12 +4,12 @@ module.exports = {
   ownerId: '1186506712040099850',
   status: {
     rotateDefault: [
-      { name: '/help', type: ActivityType.Watching },
-      { name: '/help', type: ActivityType.Playing },
+      { name: 'SB', type: ActivityType.Watching },
+      { name: 'SB', type: ActivityType.Playing },
       { name: 'SB', type: ActivityType.Streaming, url: 'https://youtube.com/@TheSBGamesOG' },
       { name: 'SB', type: ActivityType.Listening },
     ],
-    songStatus: true
+    songStatus: false
   },
   spotifyClientId: "f71a3da30e254962965ca2a89d6f74b9",
   spotifyClientSecret: "199a619d22dd4e55a4a2c1a7a3d70e63",
